5cc2bf55cb593cef6a96defa2dc0e3e7e57cef63
Shocker55 <bialetti360@gmail.com>
2023-07-25 17:04:40 +0900

    feat: SSRで記事一覧機能の実装

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,6 +1,14 @@
 import Link from "next/link";
 
-const Blogs = () => {
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMEIN}/blogs`);
+  // const res = await fetch("http://localhost:3010/blogs");
+  const blogs = await res.json();
+
+  return { props: { blogs } };
+}
+
+const Blogs = ({ blogs }) => {
   return (
     <div className="mx-auto w-[1000px]">
       <h1>ブログ一覧画面</h1>
@@ -13,6 +21,17 @@ const Blogs = () => {
         <Link href="/create-blog" className="font-medium text-blue-600 hover:underline">
           記事作成画面
         </Link>
+      </div>
+      <div>
+        <ul>
+          {blogs?.map((blog) => (
+            <Link href={`/blogs/${blog.id}`} key={blog.id}>
+              <li className="m-1 border p-3 font-medium text-blue-600 hover:underline">
+                {blog.title}
+              </li>
+            </Link>
+          ))}
+        </ul>
       </div>
     </div>
   );
