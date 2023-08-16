6412956c27f2eb7b99ec25ca307c2e3ac3de64a4
Shocker55 <bialetti360@gmail.com>
2023-08-16 21:13:07 +0900

    style: Homeページにスタイリングを追加し、雛形を作成

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,7 +8,19 @@ export default function Feed({ records }) {
         <h2 className="text-xl font-bold">Home</h2>
       </div>
 
-      {/* Post */}
+      <div className="mx-5 my-2 flex h-[200px] w-[418px] space-x-4 overflow-x-scroll rounded-lg border bg-blue-200 lg:w-[844px]">
+        <div className="h-full min-w-[300px] rounded-lg bg-blue-100">image</div>
+        <div className="h-full min-w-[300px] rounded-lg bg-blue-100">image</div>
+        <div className="h-full min-w-[300px] rounded-lg bg-blue-100">image</div>
+        <div className="h-full min-w-[300px] rounded-lg bg-blue-100">image</div>
+      </div>
+
+      {/* Record */}
+      <div className="mb-3 ml-1 font-bold">Records</div>
+      <div className="mb-1 ml-4 flex max-w-[450px] space-x-10">
+        <div className="underline">Latest Record</div>
+        <div className="text-slate-500">Popular</div>
+      </div>
       <div className="flex flex-wrap sm:w-[450px] lg:w-[900px]">
         {records?.map((record) => {
           return <RecordCard key={record.id} record={record} />;
