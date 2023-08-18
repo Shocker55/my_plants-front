855e0abd55505d398aaa442dd3ee8d302e233504
Shocker55 <bialetti360@gmail.com>
2023-08-18 15:17:07 +0900

    fix: Link、Imageコンポーネントの修正

--- src/components/RecordCard.jsx
+++ src/components/RecordCard.jsx
@@ -4,16 +4,15 @@ import { FaArrowUpFromBracket, FaCircleUser, FaRegHeart } from "react-icons/fa6"
 
 export default function RecordCard({ record, userPage }) {
   return (
-    <Link
-      href={`/records/${record.id}`}
-      className="record-card-color mx-1 my-3 flex w-[440px] justify-between rounded-lg border border-slate-300"
-    >
+    <div className="record-card-color mx-1 my-3 flex w-[440px] justify-between rounded-lg border border-slate-300">
       <div className="w-[290px]">
         <div className="flex py-2">
-          <div className="mr-3 pl-3">
-            <h3 className="truncate font-bold">{record.title}</h3>
-            <p className="mb-2 line-clamp-2 h-14 overflow-hidden py-2">{record.body}</p>
-          </div>
+          <Link href={`/records/${record.id}`}>
+            <div className="mr-3 pl-3">
+              <h3 className="truncate font-bold">{record.title}</h3>
+              <p className="mb-2 line-clamp-2 h-14 overflow-hidden py-2">{record.body}</p>
+            </div>
+          </Link>
         </div>
         {!userPage ? (
           <div className="flex items-center justify-between px-2 py-1">
@@ -25,10 +24,10 @@ export default function RecordCard({ record, userPage }) {
                     width={70}
                     height={70}
                     alt=""
-                    className="mr-3 rounded-full"
+                    className="mr-3 h-auto rounded-full"
                   />
                 ) : (
-                  <FaCircleUser className="text-5xl text-gray-400" />
+                  <FaCircleUser className="ml-2 mr-3 h-[70px] w-[60px] text-5xl text-gray-400" />
                 )}
                 <div className="px-2 text-sm font-semibold text-gray-500">
                   {record.user.profile.name}
@@ -45,24 +44,26 @@ export default function RecordCard({ record, userPage }) {
         )}
       </div>
       <div className="flex items-center justify-center pr-2">
-        {record.image.url ? (
-          <Image
-            src={record.image.url}
-            alt=""
-            width={150}
-            height={150}
-            className="h-[150px] w-[150px] rounded"
-          />
-        ) : (
-          <Image
-            src="/images/photo_icon.png"
-            alt=""
-            width={150}
-            height={150}
-            className="h-[120px] w-[120px] rounded"
-          />
-        )}
+        <Link href={`/records/${record.id}`}>
+          {record.image.url ? (
+            <Image
+              src={record.image.url}
+              alt=""
+              width={150}
+              height={150}
+              className="h-auto w-[150px] rounded"
+            />
+          ) : (
+            <Image
+              src="/images/photo_icon.png"
+              alt=""
+              width={120}
+              height={120}
+              className="h-auto w-[150px] rounded p-8"
+            />
+          )}
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 }
