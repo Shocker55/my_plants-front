a7f1b432deadc51e7418e50f589f668807752d85
Shocker55 <bialetti360@gmail.com>
2023-08-18 11:48:30 +0900

    style: 記録詳細画面のスタイリング

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -19,30 +19,32 @@ export default function Feed({ pageTitle, list, user }) {
       ) : null}
 
       {pageTitle === "ユーザー" ? (
-        <div className="flex sm:w-[450px] lg:w-[900px]">
-          <div className="mx-2 my-2 flex h-[180px] w-[418px] rounded-lg border bg-blue-200 py-3 lg:w-[844px]">
-            <div>
-              {user.avatar.url ? (
-                <Image
-                  src={user.avatar.url}
-                  alt=""
-                  width={130}
-                  height={130}
-                  className="min-h-[120px] min-w-[120px] rounded-full p-2"
-                />
-              ) : (
-                <Image
-                  src="/images/photo_icon.png"
-                  alt=""
-                  width={150}
-                  height={150}
-                  className="h-[120px] w-[120px] rounded"
-                />
-              )}
-            </div>
-            <div className="pl-3">
-              <div className="font-bold">{user.name}</div>
-              <div>{user.bio}</div>
+        <div className="flex h-[180px] justify-center sm:w-[450px] lg:w-[900px]">
+          <div className="mx-1 h-[180px] w-[450px] lg:w-[844px] ">
+            <div className="my-2 flex rounded-lg border bg-blue-200 py-3 lg:w-[844px]">
+              <div>
+                {user.avatar.url ? (
+                  <Image
+                    src={user.avatar.url}
+                    alt=""
+                    width={130}
+                    height={130}
+                    className="min-h-[120px] min-w-[120px] rounded-full p-2"
+                  />
+                ) : (
+                  <Image
+                    src="/images/photo_icon.png"
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="h-[120px] w-[120px] rounded"
+                  />
+                )}
+              </div>
+              <div className="pl-3">
+                <div className="font-bold">{user.name}</div>
+                <div>{user.bio}</div>
+              </div>
             </div>
           </div>
         </div>
