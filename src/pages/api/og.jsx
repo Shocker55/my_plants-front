0bd1f977de2554b0887886670ace65a06a119bab
Shocker55 <bialetti360@gmail.com>
2023-10-27 03:56:11 +0900

    fix:@vercel/ogで漢字も読み込めるようにアセットの追加、ogpのレイアウトの作成

--- src/pages/api/og.jsx
+++ src/pages/api/og.jsx
@@ -3,19 +3,26 @@ import { ImageResponse } from "@vercel/og";
 export const config = {
   runtime: "edge",
 };
+const fontURL = new URL(
+  `https://${process.env.NEXT_PUBLIC_S3_BUCCKET}/assets/subset_font.ttf`,
+  import.meta.url
+);
 
-export default function handler(req) {
+const font = fetch(fontURL).then((res) => res.arrayBuffer());
+
+export default async function handler(req) {
   try {
     const { searchParams } = new URL(req.url);
+    const fontData = await font;
 
     const hasTitle = searchParams.has("title");
-    const title = hasTitle ? searchParams.get("title")?.slice(0, 100) : "Default Title";
+    const title = hasTitle ? searchParams.get("title")?.slice(0, 100) : undefined;
 
     return new ImageResponse(
       (
         <div
           style={{
-            // backgroundImage: "url(https://og-image-example.vercel.app/bg.png)",
+            backgroundImage: `url(https://${process.env.NEXT_PUBLIC_S3_BUCCKET}/ogp/ogp-background.png)`,
             backgroundColor: "#fff",
             backgroundSize: "100% 100%",
             height: "100%",
@@ -28,39 +35,65 @@ export default function handler(req) {
             flexWrap: "nowrap",
           }}
         >
-          <div
-            style={{
-              width: "100%",
-              fontSize: 60,
-              fontStyle: "normal",
-              fontWeight: "bold",
-              color: "#000",
-              padding: "0 120px",
-              lineHeight: 1.3,
-              marginBottom: "30px",
-              wordWrap: "break-word",
-            }}
-          >
-            {title}
-          </div>
-          <div
-            style={{
-              width: "100%",
-              fontSize: 40,
-              fontStyle: "normal",
-              fontWeight: "bold",
-              color: "#000",
-              padding: "0 120px",
-              lineHeight: 1.3,
-            }}
-          >
-            🌿 My Plants
-          </div>
+          {title ? (
+            <div
+              style={{
+                width: "100%",
+                fontSize: 60,
+                fontStyle: "normal",
+                fontWeight: "bold",
+                color: "#000",
+                padding: "0 120px",
+                lineHeight: 1.3,
+                marginBottom: "30px",
+                wordWrap: "break-word",
+              }}
+            >
+              {title}
+            </div>
+          ) : (
+            <div
+              style={{
+                width: "100%",
+                fontSize: 80,
+                fontStyle: "normal",
+                fontWeight: "bold",
+                color: "#000",
+                marginBottom: "30px",
+                wordWrap: "break-word",
+                justifyContent: "center",
+              }}
+            >
+              My Plants
+            </div>
+          )}
+          {title ? (
+            <div
+              style={{
+                width: "100%",
+                fontSize: 40,
+                fontStyle: "normal",
+                fontWeight: "bold",
+                color: "#000",
+                padding: "0 120px",
+                lineHeight: 1.3,
+              }}
+            >
+              🌿 My Plants
+            </div>
+          ) : null}
         </div>
       ),
       {
         width: 1200,
         height: 630,
+        fonts: [
+          {
+            name: "NotoSansJP",
+            data: fontData,
+            style: "normal",
+          },
+        ],
       }
     );
   } catch (e) {
