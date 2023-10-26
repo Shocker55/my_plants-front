7cbcda2cf4efcf5706c69e0fcee6c95fd18547a0
Shocker55 <bialetti360@gmail.com>
2023-10-26 15:18:30 +0900

    fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,11 @@
 import { AuthProvider } from "@/context/AuthContext";
+import Meta from "@/components/Meta";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Meta />
       <Component {...pageProps} />
     </AuthProvider>
   );
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,11 +1,9 @@
-import Meta from "@/components/Meta";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Meta />
       <body>
         <Main />
         <NextScript />
